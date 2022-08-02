89fb4f78f3449bc0c5f2924211a51555e00e3088
Cathal O Cuinneagain <cathal1990@gmail.com>
2022-08-02 13:16:41 +0100

    completed incrementString

--- javascript/incrememntString.js
+++ javascript/incrememntString.js
@@ -1,5 +1,23 @@
+// Your job is to write a function which increments a string, to create a new string.
+
+// If the string already ends with a number, the number should be incremented by 1.
+// If the string does not end with a number. the number 1 should be appended to the new string.
+// Examples:
+
+// foo -> foo1
+
+// foobar23 -> foobar24
+
+// foo0042 -> foo0043
+
+// foo9 -> foo10
+
+// foo099 -> foo100
+
+// Attention: If the number has leading zeros the amount of digits should be considered.
+
+
 function incrementString (str) {
-    debugger;
     if (!str) {
         return '1';
     }
@@ -18,8 +36,6 @@ function incrementString (str) {
             return str.slice(0, -number.length) + number.slice(0, number.length - 1) + '1';
         }
 
-        return str.slice(0, -number.split('').length) + (parsedNumber += 1).toString()
+        return str.slice(0, -number.split('').length) + (parsedNumber += 1).toString().padStart(number.length, '0')
     }
 
-
-    console.log(incrementString('foobar001'))
